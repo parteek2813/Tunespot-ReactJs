66d66827f81b232f2fc3aad84d186ea07ce4cd0a
parteek kumar <digitalparteek@gmail.com>
2023-06-22 13:39:14 +0530

    First Commit - Basic Structure setup Complete

---
+++ src/screens/trending.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Trending = () => {
+  return <div>T</div>;
+};
+
+export default Trending;
---
+++ src/screens/userFeed.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserFeed = () => {
+  return <div>userFeed</div>;
+};
+
+export default UserFeed;
