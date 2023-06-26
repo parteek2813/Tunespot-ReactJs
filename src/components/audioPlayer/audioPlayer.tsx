b9f60556ddd40df91ea7172bb4e0d4c97f29268a
parteek kumar <digitalparteek@gmail.com>
2023-06-26 19:16:47 +0530

    Completed audioplayer page

--- src/components/audioPlayer/audioPlayer.tsx
+++ src/components/audioPlayer/audioPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./audioPlayer.css";
 import ProgressCircle from "./progressCircle";
 import WaveAnimation from "./waveAnimation";
@@ -27,20 +27,102 @@ type BigProps = TracksProps & currentTrack & audioPlayerProps;
 const AudioPlayer: React.FC<BigProps> = (props) => {
   const { currentTrack, currentIndex, setCurrentIndex, total } = props;
 
-  console.log(total);
+  console.log(total.length); // getting tracks array here
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackProgress, setTrackProgress] = useState(0);
-  var audioSource = total[0]?.preview_url;
-  const audioRef = useRef(new Audio());
+  var audioSource = total[currentIndex]?.preview_url;
 
-  const intervalRef = useRef();
+  // 0th index audio will be played initially
+  const audioRef = useRef(new Audio(total[0]?.preview_url));
+  const intervalRef: any = useRef();
 
   const isReady = useRef(false);
 
   const { duration } = audioRef.current;
   const currentPercentage = duration ? (trackProgress / duration) * 100 : 0;
 
-  console.log(currentTrack);
+  // clear any interval present already
+  const startTimer = () => {
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
+        handleNext();
+      } else {
+        setTrackProgress(audioRef.current.currentTime);
+      }
+    }, 1000);
+  };
+
+  // if audio is already playing and play is pressed
+  useEffect(() => {
+    if (audioRef.current.src) {
+      if (isPlaying) {
+        audioRef.current.play();
+        startTimer();
+      } else {
+        clearInterval(intervalRef.current);
+        audioRef.current.pause();
+      }
+    } else {
+      if (isPlaying) {
+        audioRef.current = new Audio(audioSource);
+        audioRef.current.play();
+        startTimer();
+      } else {
+        clearInterval(intervalRef.current);
+        audioRef.current.pause();
+      }
+    }
+  }, [isPlaying]);
+
+  // whenever the currentIndex changes , we are pausing the prev song
+  // and playing the next song
+  useEffect(() => {
+    audioRef.current.pause();
+    audioRef.current = new Audio(audioSource);
+
+    // before playing the next song
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      startTimer();
+    } else {
+      isReady.current = true;
+    }
+  }, [currentIndex]);
+  //   console.log(currentTrack);
+
+  // cleanup useEffect and tracks
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  // handlenext and handlePrev
+  const handleNext = () => {
+    if (currentIndex < total.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex - 1 < 0) {
+      setCurrentIndex(total.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const addZero = (n: number) => {
+    return n > 9 ? "" + n : "0" + n;
+  };
 
   const artist: any[] = [];
   // name location: currentTrack.artists[0].name
@@ -67,16 +149,16 @@ const AudioPlayer: React.FC<BigProps> = (props) => {
 
         <div className="player-right-bottom flex">
           <div className="song-duration flex">
-            <p className="duration">0:30</p>
-            <WaveAnimation isPlaying={true} />
+            <p className="duration">0:{addZero(Math.round(trackProgress))}</p>
+            <WaveAnimation isPlaying={isPlaying} />
             <p className="duration">0:30</p>
           </div>
           <Controls
-          // isPlaying={isPlaying}
-          // setIsPlaying={setIsPlaying}
-          // handleNext={handleNext}
-          // handlePrev={handlePrev}
-          // total={total}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            handleNext={handleNext}
+            handlePrev={handlePrev}
+            total={total.length}
           />
         </div>
       </div>
--- src/components/audioPlayer/controls.tsx
+++ src/components/audioPlayer/controls.tsx
@@ -1,8 +1,43 @@
 import React from "react";
 import "./controls.css";
+import { IconContext } from "react-icons";
+import { FaPause } from "react-icons/fa";
+import { IoPlaySkipBack, IoPlaySkipForward, IoPlay } from "react-icons/io5";
 
-const Controls = () => {
-  return <div>Controls</div>;
+interface ControlProps {
+  isPlaying: boolean | any;
+  setIsPlaying: boolean | any;
+  handleNext: boolean | any;
+  handlePrev: boolean | any;
+  total: number;
+}
+
+const Controls: React.FC<ControlProps> = (props) => {
+  const { isPlaying, setIsPlaying, handleNext, handlePrev, total } = props;
+  console.log(isPlaying); // true
+  return (
+    <>
+      <IconContext.Provider value={{ size: "35px", color: "#000000" }}>
+        <div className="controls-wrapper flex">
+          <div className="action-btn flex" onClick={handlePrev}>
+            <IoPlaySkipBack />
+          </div>
+          <div
+            className={
+              isPlaying ? "play-pause-btn flex active" : "play-pause-btn flex"
+            }
+            onClick={() => setIsPlaying(!isPlaying)}
+          >
+            {isPlaying ? <FaPause /> : <IoPlay />}
+          </div>
+          <div className="action-btn flex" onClick={handleNext}>
+            <IoPlaySkipForward />
+          </div>
+        </div>
+      </IconContext.Provider>
+      <p className="total"> Total Songs: {total} </p>
+    </>
+  );
 };
 
 export default Controls;
